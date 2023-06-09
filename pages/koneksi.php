48ae2bcead590716de8c1809bf6b25f922a97718
Muhammad Maulana Kharyska Abadi <107461887+mokhaaaa@users.noreply.github.com>
2023-06-09 17:45:20 +0700

    Delete pages directory

--- pages/koneksi.php
+++
@@ -1,8 +0,0 @@
-<?php
-        $dbHost = 'localhost:3307';
-        $dbUser = 'root';
-        $dbPass = '';
-        $dbName = 'adventureworks-dw';
-
-        $conn = mysqli_connect($dbHost, $dbUser, $dbPass, $dbName);
-?>
